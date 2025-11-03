6a1ca831623cc09f1e27ee36cad651f0a6034469
aldrnkrlprdo <aldrnkrlprdo@gmail.com>
2025-11-04 03:59:43 +0800

    fix index.js

--- api/index.js
+++ api/index.js
@@ -1,28 +1,40 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const app = express();
-const userRoutes = require('./routes/user.routes');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3001;
+const userRoutes = require('./routes/user.routes');
+const { swaggerUi, specs } = require('./swagger');
 
+const app = express();
 app.use(express.json());
 
-// DB Connection
-const connectDB = require('./config/db.config');
-connectDB();
-
 // Swagger
-const { swaggerUi, specs } = require('./swagger');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 // Routes
 app.use('/api/users', userRoutes);
 
-// Global error handler (optional)
+// Global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
-module.exports = serverless(app);
+// Safe DB connection wrapper
+let isConnected = false;
+const connectDB = async () => {
+  if (!isConnected) {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+    console.log('MongoDB connected');
+  }
+};
+
+// Wrap Express with serverless
+const handler = async (req, res) => {
+  await connectDB(); // connect only when function is invoked
+  return serverless(app)(req, res);
+};
+
+module.exports = handler;
