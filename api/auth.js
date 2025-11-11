1c0addc11df964cadb992ebd00b709e11ac16aaa
aldrnkrlprdo <aldrnkrlprdo@gmail.com>
2025-11-11 23:52:39 +0800

    add json limit in auth js

--- api/auth.js
+++ api/auth.js
@@ -8,7 +8,8 @@ const connectDB = require('./lib/db');
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 
 // CORS setup (use ALLOWED_ORIGINS env var, comma-separated)
