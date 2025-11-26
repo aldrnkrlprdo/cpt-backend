e474516f44b8c023efe367e3a4a6fec4ff362400
aldrnkrlprdo <aldrnkrlprdo@gmail.com>
2025-11-27 02:29:17 +0800

    align schema

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  firstname: String,
-  lastname: String,
+  firstName: String,
+  lastName: String,
   username: { type: String, unique: true },
   email: { type: String, unique: true },
   password: String,
