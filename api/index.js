0962e78c4422bff1184a47cd6a88c829d56f1749
aldrnkrlprdo <aldrnkrlprdo@gmail.com>
2025-11-27 01:08:38 +0800

    fix 404 in prod

--- api/index.js
+++ api/index.js
@@ -50,4 +50,4 @@ app.use((err, req, res, next) => {
 // Connect DB once per container
 connectDB().catch(console.error);
 
-module.exports = serverless(app);
+module.exports.handler = serverless(app);
