5a21f9545e149bb5a92377c3103f7c7faf4ffdaa
aldrnkrlprdo <aldrnkrlprdo@gmail.com>
2026-01-10 20:51:05 +0800

    updating fields in user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  employeeId: String,
   firstName: String,
   lastName: String,
   username: { type: String, unique: true },
   email: { type: String, unique: true },
   password: String,
   role: { type: String, default: 'user' },
-  status: { type: String, default: 'active' },
+  membershipStatus: { type: String, default: 'active' },
+  dateOfJoining: { type: Date, default: Date.now },
+  address: String,
+  phoneNumber: String,
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
