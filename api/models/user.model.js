a0cc4651a03ac8a20fa22aed4ef4336d3ade2efa
aldrnkrlprdo <aldrnkrlprdo@gmail.com>
2026-01-10 20:52:21 +0800

    reverting changes

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  employeeId: String,
   firstName: String,
   lastName: String,
   username: { type: String, unique: true },
   email: { type: String, unique: true },
   password: String,
   role: { type: String, default: 'user' },
-  membershipStatus: { type: String, default: 'active' },
-  dateOfJoining: { type: Date, default: Date.now },
-  address: String,
-  phoneNumber: String,
+  status: { type: String, default: 'active' },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
