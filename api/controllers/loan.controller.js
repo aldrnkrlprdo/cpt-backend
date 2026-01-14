78b4a4365884f42a07326d0e88a7a1e69f467a06
aldrnkrlprdo <aldrnkrlprdo@gmail.com>
2026-01-15 03:06:15 +0800

    add id validation in updateloan

--- api/controllers/loan.controller.js
+++ api/controllers/loan.controller.js
@@ -69,7 +69,12 @@ exports.updateLoan = async (req, res) => {
   try {
     await connectDB();
     const { id } = req.params;
-    const updatedLoan = await Loan.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
+
+    const query = mongoose.Types.ObjectId.isValid(id)
+          ? { _id: id }
+          : { loanId: id };
+
+    const updatedLoan = await Loan.findByIdAndUpdate(query, req.body, { new: true, runValidators: true });
     if (!updatedLoan) return res.status(404).json({ error: 'Loan not found' });
     res.json({ message: 'Loan updated successfully', loan: updatedLoan });
   } catch (err) {
