0ed704e61c48a4f4a591bd7823cfd1ba663ec211
aldrnkrlprdo <aldrnkrlprdo@gmail.com>
2026-01-15 03:15:29 +0800

    update payload

--- api/controllers/loan.controller.js
+++ api/controllers/loan.controller.js
@@ -69,12 +69,13 @@ exports.updateLoan = async (req, res) => {
   try {
     await connectDB();
     const { id } = req.params;
-    
+    const {loanId, ...payload} = req.body;
+
     const query = mongoose.Types.ObjectId.isValid(id)
-          ? { _id: id }
-          : { loanId: id };
-console.log(query)
-    const updatedLoan = await Loan.findByIdAndUpdate(query, req.body, { new: true, runValidators: true });
+      ? { _id: id }
+      : { loanId: id };
+    console.log(query)
+    const updatedLoan = await Loan.findByIdAndUpdate(query, payload, { new: true, runValidators: true });
     if (!updatedLoan) return res.status(404).json({ error: 'Loan not found' });
     res.json({ message: 'Loan updated successfully', loan: updatedLoan });
   } catch (err) {
@@ -99,7 +100,7 @@ exports.deleteLoan = async (req, res) => {
 exports.getAllLoansByEmployeeId = async (req, res) => {
   try {
     await connectDB();
-    
+
     const { employeeId } = req.params;
 
     // Use the employee's Id to find their loans
