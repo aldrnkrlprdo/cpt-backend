47adda6cf3d95503642460241304c4475e59c55b
aldrnkrlprdo <aldrnkrlprdo@gmail.com>
2026-01-15 02:57:50 +0800

    change query to findoneandupdate

--- api/controllers/loan.controller.js
+++ api/controllers/loan.controller.js
@@ -69,7 +69,7 @@ exports.updateLoan = async (req, res) => {
   try {
     await connectDB();
     const { loanId } = req.params;
-    const updatedLoan = await Loan.findByIdAndUpdate({loanId: loanId}, req.body, { new: true, runValidators: true });
+    const updatedLoan = await Loan.findOneAndUpdate({loanId: loanId}, req.body, { new: true, runValidators: true });
     if (!updatedLoan) return res.status(404).json({ error: 'Loan not found' });
     res.json({ message: 'Loan updated successfully', loan: updatedLoan });
   } catch (err) {
