f8b4a476eae88ced362ba99816ffe7c896bcb203
aldrnkrlprdo <aldrnkrlprdo@gmail.com>
2026-01-15 03:17:37 +0800

    debug

--- api/controllers/loan.controller.js
+++ api/controllers/loan.controller.js
@@ -74,7 +74,7 @@ exports.updateLoan = async (req, res) => {
     const query = mongoose.Types.ObjectId.isValid(id)
       ? { _id: id }
       : { loanId: id };
-    console.log(query)
+    console.log(query, payload, req.body)
     const updatedLoan = await Loan.findByIdAndUpdate(query, payload, { new: true, runValidators: true });
     if (!updatedLoan) return res.status(404).json({ error: 'Loan not found' });
     res.json({ message: 'Loan updated successfully', loan: updatedLoan });
