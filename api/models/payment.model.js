5a981436d4bb46655205850bb10f7030a7ea672d
aldrnkrlprdo <aldrnkrlprdo@gmail.com>
2026-02-09 02:47:50 +0800

    update paymentschema

--- api/models/payment.model.js
+++ api/models/payment.model.js
@@ -13,9 +13,8 @@ const paymentSchema = new mongoose.Schema({
     required: true,
   },
   loanId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Loan',
-    required: true,
+    type: String,
+    ref: 'Loan'
   },
   paymentDate: {
     type: Date,
@@ -26,6 +25,23 @@ const paymentSchema = new mongoose.Schema({
     required: true,
   },
 }, { timestamps: true });
+
+paymentSchema.pre('save', async function (next) {
+  if (this.isNew && !this.paymentId) {
+    const Payment = this.constructor;
+    const lastPayment = await Payment.findOne({}, {}, { sort: { 'createdAt': -1 } });
+
+    let nextIdNum;
+    if (lastPayment && lastPayment.paymentId) {
+      const lastIdNum = parseInt(lastPayment.paymentId, 10);
+      nextIdNum = lastIdNum + 1;
+    } else {
+      nextIdNum = '000001'; // Start from 1
+    }
+    this.paymentId = nextIdNum;
+  }
+  next();
+});
 
 const Payment = mongoose.model('Payment', paymentSchema);
 
